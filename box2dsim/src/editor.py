c76d0ed8113969151d3e00a07122c238ce2a2b4a
francesco <francesco@localhost.localdomain>
2017-06-18 23:49:34 +0200

    editor

--- box2dsim/src/editor.py
+++ box2dsim/src/editor.py
@@ -6,6 +6,7 @@ import json
 from PySide import QtCore, QtGui
 import Box2D as b2
 from Box2D.Box2D import b2_dynamicBody
+from prompt_toolkit.layout import toolbars
 
 
 class DataManager(object):
@@ -47,12 +48,13 @@ class MainWindow(QtGui.QMainWindow):
         toolBar = QtGui.QToolBar(self)
         self.addToolBar(toolBar)
    
+        # main commands
+        
         exitAction = QtGui.QAction('Exit', self)
         exitAction.setShortcut('Q')
         exitAction.triggered.connect(self.close)
         toolBar.addAction(exitAction)
  
-
         saveAction = QtGui.QAction('Save', self)
         saveAction.setShortcut('S')
         saveAction.triggered.connect(self.save)     
@@ -61,135 +63,114 @@ class MainWindow(QtGui.QMainWindow):
         loadAction = QtGui.QAction('Load', self)
         loadAction.setShortcut('L')
         loadAction.triggered.connect(self.load) 
+        
         toolBar.addAction(loadAction)
+        toolBar.addSeparator()
 
+        # navigate/create/delete objects 
+                
+        newAction = QtGui.QAction('New', self)
+        newAction.setShortcut('N')
+        newAction.triggered.connect(self.newObject)    
+        toolBar.addAction(newAction)
+                
+        prevAction = QtGui.QAction('Prev', self)
+        prevAction.setShortcut('Left')
+        prevAction.triggered.connect(self.prevObject)     
+        toolBar.addAction(prevAction)
+         
+        nextAction = QtGui.QAction('Next', self)
+        nextAction.setShortcut('Right')
+        nextAction.triggered.connect(self.nextObject)   
+        toolBar.addAction(nextAction)
+ 
+        deleteAction = QtGui.QAction('delete', self)
+        deleteAction.setShortcut('Delete')
+        deleteAction.triggered.connect(self.deleteObject)    
+        toolBar.addAction(deleteAction)
+        
+        toolBar.addSeparator()
+      
+        objects = QtGui.QActionGroup(self)
+        objects.setExclusive(True)
+        
+        polygonAction = QtGui.QAction('Polygon', self)
+        polygonAction.setShortcut('P')
+        polygonAction.setCheckable(True)
+        polygonAction.setChecked(True)
+        polygonAction.triggered.connect(self.onPolygons)         
+        toolBar.addAction(polygonAction)
+        objects.addAction(polygonAction)
+   
+        circleAction = QtGui.QAction('Circle', self)
+        circleAction.setShortcut('C')
+        circleAction.setCheckable(True)
+        circleAction.triggered.connect(self.onCircles)         
+        toolBar.addAction(circleAction)
+        objects.addAction(circleAction)
+        
+        jointAction = QtGui.QAction('Joint', self)
+        jointAction.setShortcut('J')
+        jointAction.setCheckable(True)
+        jointAction.triggered.connect(self.onJoints)        
+        toolBar.addAction(jointAction)
+        objects.addAction(jointAction)
+        
+        self.addToolBarBreak()
+        manualBar = QtGui.QToolBar(self)
+        self.addToolBar(manualBar)
+        
 
+        polygonManual = QtGui.QActionGroup(self)
+        polygonManual.setExclusive(True)
+     
+        addPointAction = QtGui.QAction('+', self)
+        addPointAction.setShortcut('+')
+        addPointAction.setCheckable(True)
+        addPointAction.setChecked(True)
+        addPointAction.triggered.connect(self.onAddPoints)         
+        manualBar.addAction(addPointAction)
+        polygonManual.addAction(addPointAction)   
+
+        delPointAction = QtGui.QAction('-', self)
+        delPointAction.setShortcut('-')
+        delPointAction.setCheckable(True)
+        delPointAction.triggered.connect(self.onDelPoints)         
+        manualBar.addAction(delPointAction)
+        polygonManual.addAction(delPointAction)  
+        
     def save(self):
         print "Save"
     
     def load(self):
         print "Load"
         
-#         polygonAction = QtGui.QAction('Polygon', self)
-#         polygonAction.setShortcut('P')
-#         polygonAction.setCheckable(True)
-#         polygonAction.setChecked(True)
-#         polygonAction.triggered.connect(self.drawing_frame.onPolygons)         
-#         circleAction = QtGui.QAction('Circle', self)
-#         circleAction.setShortcut('C')
-#         circleAction.setCheckable(True)
-#         circleAction.triggered.connect(self.drawing_frame.onCircles)         
-#         jointAction = QtGui.QAction('Joint', self)
-#         jointAction.setShortcut('C')
-#         jointAction.setCheckable(True)
-#         jointAction.triggered.connect(self.drawing_frame.onJoints)         
-#         
-#         newAction = QtGui.QAction('New', self)
-#         newAction.setShortcut('N')
-#         newAction.triggered.connect(self.drawing_frame.newShape)    
-  
-#                        
-#         prevAction = QtGui.QAction('Prev', self)
-#         prevAction.setShortcut('Left')
-#         prevAction.triggered.connect(self.drawing_frame.prevShape)       
-#         nextAction = QtGui.QAction('Next', self)
-#         nextAction.setShortcut('Right')
-#         nextAction.triggered.connect(self.drawing_frame.nextShape)   
-#         
-#         self.addPointsAction = QtGui.QAction('glue+', self)
-#         self.addPointsAction.setShortcut('+')
-#         self.addPointsAction.setCheckable(True)
-#         self.addPointsAction.setChecked(True)
-#         self.addPointsAction.triggered.connect(self.drawing_frame.onGluePlus)         
-# 
-#         self.delPointsAction = QtGui.QAction('glue-', self)
-#         self.delPointsAction.setShortcut('-')
-#         self.delPointsAction.setCheckable(True)
-#         self.delPointsAction.triggered.connect(self.drawing_frame.onGlueMinus)         
-#    
-#         self.manualPolyModeAction = QtGui.QAction('Manual', self)
-#         self.manualPolyModeAction.setShortcut('M')
-#         self.manualPolyModeAction.setCheckable(True)
-#         self.manualPolyModeAction.setChecked(True)
-#         self.manualPolyModeAction.triggered.connect(self.drawing_frame.onPolyModeManual)         
-# 
-#         self.regularPolyModeAction = QtGui.QAction('Regular', self)
-#         self.regularPolyModeAction.setShortcut('R')
-#         self.regularPolyModeAction.setCheckable(True)
-#         self.regularPolyModeAction.triggered.connect(self.drawing_frame.onPolyModeRegular)         
-#         
-#         self.vrtsBox = QtGui.QComboBox(self)
-#         self.vrtsBox.addItems(["%d" % x for x in range(3,17) ])
-#         self.vrtsBox.currentIndexChanged.connect(self.drawing_frame.onVrtxBoxChanged)
-#         self.vrtsBox.setEnabled(False)    
-#         self.angleBox = QtGui.QLineEdit(self)
-#         validator = QtGui.QIntValidator()
-#         self.angleBox.setValidator(validator)
-#         self.angleBox.setMaxLength(4)   
-#         self.angleBox.setEnabled(False)    
-#         self.angleBox.textChanged.connect(self.drawing_frame.onAngleBoxChanged)
-#         self.angleLabel = QtGui.QLabel("angle")
-#         self.angleLabel.setEnabled(False)    
-#         self.radiusBox = QtGui.QLineEdit(self)
-#         validator = QtGui.QIntValidator()
-#         self.radiusBox.setValidator(validator)
-#         self.radiusBox.setMaxLength(4)   
-#         self.radiusBox.setEnabled(False)    
-#         self.radiusBox.textChanged.connect(self.drawing_frame.onRadiusBoxChanged)
-#         self.radiusLabel = QtGui.QLabel("radius")
-#         self.radiusLabel.setEnabled(False)    
-# 
-#         shapes = QtGui.QActionGroup(self)
-#         shapes.addAction(polygonAction)
-#         shapes.addAction(circleAction)
-#         shapes.addAction(jointAction)
-#         shapes.setExclusive(True)
-#         
-#         self.actionsGroup = QtGui.QActionGroup(self)
-#         self.actionsGroup.addAction(self.addPointsAction)
-#         self.actionsGroup.addAction(self.delPointsAction)   
-#         
-#         self.polyModeGroup = QtGui.QActionGroup(self)
-#         self.polyModeGroup.addAction(self.manualPolyModeAction)
-#         self.polyModeGroup.addAction(self.regularPolyModeAction)
-# 
-#         
-
-#         toolBar.addSeparator()
-#         toolBar.addAction(prevAction)
-#         toolBar.addAction(nextAction)
-#         toolBar.addSeparator()
-#         toolBar.addAction(newAction)
-#         toolBar.addAction(deleteAction)
-#         toolBar.addAction(saveAction)
-#         toolBar.addAction(loadAction)
-#         toolBar.addSeparator()
-#         toolBar.addAction(polygonAction)
-#         toolBar.addAction(circleAction)
-#         toolBar.addAction(jointAction) 
-#         toolBar.addSeparator()
-#         self.addToolBar(toolBar)
-#         self.addToolBarBreak()
-#         
-#         self.manualBar = QtGui.QToolBar(self)
-#         self.manualBar.addAction(self.manualPolyModeAction)
-#         self.manualBar.addAction(self.addPointsAction)
-#         self.manualBar.addAction(self.delPointsAction)
-#         self.addToolBar(self.manualBar)
-#           
-#         self.regularBar = QtGui.QToolBar(self)
-#         self.regularBar.addAction(self.regularPolyModeAction)
-#         self.regularBar.addWidget(self.vrtsBox)
-#         self.regularBar.addWidget(self.angleLabel)
-#         self.regularBar.addWidget(self.angleBox)
-#         self.regularBar.addWidget(self.radiusLabel)
-#         self.regularBar.addWidget(self.radiusBox)
-#         self.addToolBar(self.regularBar)
-#                
-#         self.modifyBar = QtGui.QToolBar(self)
-#         self.addToolBar(self.modifyBar)
-
-
+    def newObject(self):
+        print "New"
+    
+    def prevObject(self):
+        print "Previous"
+    
+    def nextObject(self):
+        print "next";
+    
+    def deleteObject(self):
+        print "Delete"        
+    
+    def onPolygons(self):
+        print "Poligoans"
+        
+    def onCircles(self):
+        print "Circles"
+        
+    def onJoints(self):
+        print "Joints"
+ 
+    def onAddPoints(self):
+        print "Add Points"
+        
+    def onDelPoints(self):
+        print "Del Points"   
 if __name__ == '__main__':
 
     import sys
