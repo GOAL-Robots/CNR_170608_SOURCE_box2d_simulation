62b00e0422c5fda744b7112da78ad2ddbb6881a4
francesco <francesco@localhost.localdomain>
2017-06-19 06:54:13 +0200

    edit

--- box2dsim/src/editor.py
+++ box2dsim/src/editor.py
@@ -15,7 +15,7 @@ class World(DataObject):
         
         super(World, self).__init__()
         
-        self.data["gravity"] = (0,0) 
+        self.data["gravity"] = [0,0] 
         self.data["allowSleep"] = True 
         self.data["autoClearForces"] = True 
         self.data["positionIterations"] = 2 
@@ -26,7 +26,16 @@ class World(DataObject):
         self.data["subStepping"] = False 
 
     def updateFromJson(self, jsw):
-        pass
+        
+        self.data["gravity"] = [jsw["gravity"]["x"], jsw["gravity"]["y"]]
+        self.data["allowSleep"] = True 
+        self.data["autoClearForces"] = True 
+        self.data["positionIterations"] = 2 
+        self.data["velocityIterations"] = 6 
+        self.data["stepsPerSecond"] = 60 
+        self.data["warmStarting"] = True 
+        self.data["continuousPhysics"] = True 
+        self.data["subStepping"] = False 
     
     def convertToJson(self):
         pass
