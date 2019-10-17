5a02e90ff0ecba99b64b99818c8c98e2cd80a541
Francesco Mannella <francesco.mannella@gmail.com>
2019-10-17 16:10:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='box2dsim',
             'install': MyInstall,
             'egg_info': MyEgg
             },
-        install_requires=['gym', 'box2d-py', 'numpy', 
+        install_requires=['gym', 'box2d', 'numpy', 
             'matplotlib','scikit-image']
         )
